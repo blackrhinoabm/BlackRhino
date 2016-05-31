ace43e1b2f16ab1e26b6d6165f83aa3e7f75fc39
t1nak <tinakoziol@yahoo.de>
2016-05-31 15:34:58 +0200

    Agent script improved
    Agent script after second session with Pawel where he showed how to
    implement the matrix multiplication via a temporary variable

--- examples/degroot/src/agent.py
+++ examples/degroot/src/agent.py
@@ -20,8 +20,19 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+# This script contains the Agent class which is later called in the Environment
+# script. In the degroot learning algorithm a group of individuals observe
+# each other's opinions and adapt their subjective propability distribution
+# of an unknown value of a parameter theta
+# The model (which is executed in the updater script) describes how the group
+# forms a collective subjective propability distrbution by revealing their
+# individual distribution to each other and pooling their opnion
+
+# Implementation: Every agent has an opinion variable and weights/probabilities
+# describing how much it'cares' for the opinion of the other. These weights are
+# stored in a dictionary called transition_probabilities
+
 import logging
-import os
 
 # ============================================================================
 #
@@ -37,9 +48,9 @@ class Agent(object):
     #
     #
 
-    identifier = ""  # identifier of the specific bank
+    identifier = ""  # identifier of the specific agent
     opinion = 0.0   # initial 'opinion' of the agent
-    transition_probabilities= {}  # weights of the opinions
+    transition_probabilities = {}  # weights of the opinions
 
     #
     #
@@ -47,42 +58,15 @@ class Agent(object):
     #
     #
 
-    # -------------------------------------------------------------------------
-    # functions for setting/changing id, parameters, and state variables
-    # these either return or set specific value to the above variables
-    # with the exception of append (2 last ones) which append the dictionaries
-    #
-    # -------------------------------------------------------------------------
-    # functions needed to make Agent() hashable
-    # -------------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # __init__  used to automatically instantiate an agent as an object when
+    # the agent class is called
+    # ------------------------------------------------------------------------
 
-    def __key__(self):
-        return self.identifier
-
-    def __eq__(self, other):
-        return self.__key__() == other.__key__()
-
-    def __hash__(self):
-        return hash(self.__key__())
-
-
-    # -------------------------------------------------------------------------
-    # __init__
-    # -------------------------------------------------------------------------
     def __init__(self):
-        self.identifier = ""  # identifier of the specific agent... e.g. it will be read from the config file
-        self.opinion = 0.0  # parameters of the opinion of the specific agent
-        self.transition_probabilities= {}  # transision probabilities of the specific bank
-
-        # DO NOT EVER ASSIGN PARAMETERS BY HAND AS DONE BELOW IN PRODUCTION CODE
-        # ALWAYS READ THE PARAMETERS FROM CONFIG FILES
-        # OR USE THE FUNCTIONS FOR SETTING / CHANGING VARIABLES
-        # CONVERSELY, IF YOU WANT TO READ THE VALUE, DON'T USE THE FULL NAMES
-        # INSTEAD USE __getattr__ POWER TO CHANGE THE COMMAND FROM
-        # instance.static_parameters["xyz"] TO instance.xyz - THE LATTER IS PREFERRED
-
-    # -------------------------------------------------------------------------
-
+        self.identifier = ""  # identifier of the specific agent
+        self.opinion = 0.0  # opinion of the specific agent
+        self.transition_probabilities = {} # dictionary with weights
 
     # -------------------------------------------------------------------------
     # get_parameters_from_file
@@ -93,7 +77,6 @@ class Agent(object):
     #     <parameter name='string' value='string'></parameter>
     # </agent>
     # -------------------------------------------------------------------------
-
 
     def get_parameters_from_file(self, agent_filename, environment):
         from xml.etree import ElementTree
@@ -108,28 +91,35 @@ class Agent(object):
 
             # loop over all <parameter> entries in the xml file
             for subelement in element:
-                name = subelement.attrib['name']
-                value = subelement.attrib['value']
+                if subelement.attrib['type'] == 'static':
+                    name = subelement.attrib['name']
+                    value = subelement.attrib['value']
 
-            # take 'starting opinion' from static type
-                if subelement.attrib['type']=='static':
-                    return value=float(value)
+                    if name == 'starting opinion':
+                        self.opinion = float(value)
 
-            # take weights from transition type and add them to transition probabilities list
-                if subelement.attrib['type']=='transition':
-                    self.transition_probabilities[name]=float(value)
+                if subelement.attrib['type'] == 'transition':
+                    name = subelement.attrib['name']
+                    value = subelement.attrib['value']
+                    self.transition_probabilities[name] = float(value)
 
         except:
             logging.error("    ERROR: %s could not be parsed", agent_filename)
-    # a standard function reading parameters of the agents from
-    # an xml file, looking somewhat like the below
-    # <agent identifier='string'>
-    #     <parameter name='string' value='string'></parameter>
-    # </agent>
 
-    def create_temporary_variable(self, )
-        tempv =
+    # -------------------------------------------------------------------------
+    # The next function creates a temporary variable tempv (which needs some
+    # kind of starting value, here 0.0),
+    # then we go through every agent in the list agents which is defined in the
+    # environment class (in the environment script), take its opinion variable,
+    # multiply it with the weight stored in the transition_probabilities
+    # dictionary. We also need the key agent.identifier to tell python
+    # which weight to multiply with which opinion
+    # return is needed to pass the tempv variable back to the updater script
+    # -------------------------------------------------------------------------
+    def create_temporary_variable(self, environment):
+        tempv = 0.0
 
-    def multiply_matrices
+        for agent in environment.agents:
+            tempv = tempv + agent.opinion * self.transition_probabilities[agent.identifier]
 
- return 
+        return tempv
