bf21a03bb25a6ed30169ff8d576cdcab3fb33104
t1nak <tinakoziol@yahoo.de>
2016-05-31 07:48:01 +0200

    test de groot
    trying degroot learning

---
+++ examples/degroot/src/agent.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# [SublimeLinter pep8-max-line-length:150]
+# -*- coding: utf-8 -*-
+
+"""
+black_rhino is a multi-agent simulator for financial network analysis
+Copyright (C) 2016 Co-Pierre Georg (co-pierre.georg@keble.ox.ac.uk)
+Pawel Fiedor (pawel@fiedor.eu)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+import os
+
+# ============================================================================
+#
+# class Bank
+#
+# ============================================================================
+
+
+class Agent(object):
+    #
+    #
+    # VARIABLES
+    #
+    #
+
+    identifier = ""  # identifier of the specific bank
+    opinion = 0.0   # parameters of 'opinion' of the agent
+    transition_probabilities= {}  # weights of the opinions
+
+    #
+    #
+    # CODE
+    #
+    #
+
+    # -------------------------------------------------------------------------
+    # functions for setting/changing id, parameters, and state variables
+    # these either return or set specific value to the above variables
+    # with the exception of append (2 last ones) which append the dictionaries
+    #
+    # -------------------------------------------------------------------------
+    # functions needed to make Agent() hashable
+    # -------------------------------------------------------------------------
+
+    def __key__(self):
+        return self.identifier
+
+    def __eq__(self, other):
+        return self.__key__() == other.__key__()
+
+    def __hash__(self):
+        return hash(self.__key__())
+
+
+    # -------------------------------------------------------------------------
+    # __init__
+    # -------------------------------------------------------------------------
+    def __init__(self):
+        self.identifier = ""  # identifier of the specific agent... e.g. it will be read from the config file
+        self.opinion = 0.0  # parameters of the opinion of the specific agent
+        self.transition_probabilities= {}  # transision probabilities of the specific bank
+
+        # DO NOT EVER ASSIGN PARAMETERS BY HAND AS DONE BELOW IN PRODUCTION CODE
+        # ALWAYS READ THE PARAMETERS FROM CONFIG FILES
+        # OR USE THE FUNCTIONS FOR SETTING / CHANGING VARIABLES
+        # CONVERSELY, IF YOU WANT TO READ THE VALUE, DON'T USE THE FULL NAMES
+        # INSTEAD USE __getattr__ POWER TO CHANGE THE COMMAND FROM
+        # instance.static_parameters["xyz"] TO instance.xyz - THE LATTER IS PREFERRED
+
+    # -------------------------------------------------------------------------
+
+
+    # -------------------------------------------------------------------------
+    # get_parameters_from_file
+    # reads the specified config file given the environment
+    # and sets parameters to the ones found in the config file
+    # the config file should be an xml file that looks like the below:
+    # <agent identifier='string'>
+    #     <parameter name='string' value='string'></parameter>
+    # </agent>
+    # -------------------------------------------------------------------------
+
+
+    def get_parameters_from_file(self, agent_filename, environment):
+        from xml.etree import ElementTree
+
+        try:
+            xmlText = open(agent_filename).read()
+            element = ElementTree.XML(xmlText)
+            # we get the identifier
+            self.identifier = element.attrib['identifier']
+            # and then we're only interested in <parameter> fields
+            element = element.findall('parameter')
+
+            # loop over all <parameter> entries in the xml file
+            for subelement in element:
+                name = subelement.attrib['name']
+                value=subelement.attrib['value']
+
+            # take 'starting opinion' from static type
+                if subelement.attrib['type']=='static':
+                    return value=float(value)
+
+            # take weights from transition type and add them to transition probabilities list
+                if subelement.attrib['type']=='transition':
+                    self.transition_probabilities[name]=float(value)
+
+        except:
+            logging.error("    ERROR: %s could not be parsed", agent_filename)
+    # a standard function reading parameters of the agents from
+    # an xml file, looking somewhat like the below
+    # <agent identifier='string'>
+    #     <parameter name='string' value='string'></parameter>
+    # </agent>
+
+
+
+ -temporary variable 
+ multiply matrices 
+
+ return 
--- examples/degroot/src/runner.py
+++ examples/degroot/src/runner.py
@@ -21,9 +21,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from src.updater import Updater
-from abm_template.src.baserunner import BaseRunner
-from src.measurement import Measurement
-from src.shock import Shock
 
 # -------------------------------------------------------------------------
 #
@@ -32,7 +29,7 @@ from src.shock import Shock
 # -------------------------------------------------------------------------
 
 
-class Runner(BaseRunner):
+class Runner(object):
     #
     #
     # VARIABLES
@@ -59,30 +56,6 @@ class Runner(BaseRunner):
     # -------------------------------------------------------------------------
     # get_identifier
     # -------------------------------------------------------------------------
-    def get_identifier(self):
-        return self.identifier
-    # -------------------------------------------------------------------------
-
-    # -------------------------------------------------------------------------
-    # set_identifier
-    # -------------------------------------------------------------------------
-    def set_identifier(self, _value):
-        super(Runner, self).set_identifier(_value)
-    # -------------------------------------------------------------------------
-
-    # -------------------------------------------------------------------------
-    # get_num_sweeps
-    # -------------------------------------------------------------------------
-    def get_num_sweeps(self):
-        return self.num_sweeps
-    # -------------------------------------------------------------------------
-
-    # -------------------------------------------------------------------------
-    # set_num_sweeps
-    # -------------------------------------------------------------------------
-    def set_num_sweeps(self, _value):
-        super(Runner, self).set_num_sweeps(_value)
-    # -------------------------------------------------------------------------
 
     # -------------------------------------------------------------------------
     # initialize()
@@ -98,63 +71,13 @@ class Runner(BaseRunner):
     # -------------------------------------------------------------------------
     def do_run(self, environment):
         # loop over all time steps and do the updating
-        # We initialise the measurement class for writing outputs to csv
-        # measurement = Measurement("Measurement", environment, self, {1: ["Step", "static", "self.runner.current_step"],
-        # 2: ["Deposits", "dynamic", "self.environment.households[0].get_account", ["deposits"]]}, "TestMeasurement.csv")
-        measurement = Measurement(environment, self)
-        # And open the output file
-        measurement.open_file()
-        # We start the shock class as well
-        shock_class = Shock()
+
         # For each update step
         for i in range(self.num_sweeps):
-            # Do the shock:
-            # First we check if the shock occurs at the current sweep
-            # Then we run the shock procedure at the start of the update
-            for shock in environment.shocks:
-                if int(shock[0]) <= i+1 and int(shock[1]) >= i+1:
-                    shock_class.do_shock(environment, i, shock[2], "start")
+
             # the update step
             # append current step, this is mostly for measurements
             self.current_step = i
             # do the actual update
             self.updater.do_update(environment, i)
-            # write the state of the system
-            measurement.write_to_file()
-            # Do the shock (revert the shock if necessary):
-            # First we check if the shock occurs at the current sweep
-            # Then we run the shock procedure at the end of the update
-            for shock in environment.shocks:
-                if int(shock[0]) <= i+1 and int(shock[1]) >= i+1:
-                    shock_class.do_shock(environment, i, shock[2], "end")
-            # HELPER, to be removed in production
-            # for firm in environment.households:
-            #    print(firm)
-            # print(environment.households[0])
-            # print(environment.firms[0])
-            # capital = 0.0
-            # for tranx in environment.firms[0].accounts:
-            #     if tranx.type_ == "capital" and tranx.from_ == environment.firms[0]:
-            #         capital = capital + tranx.amount
-            #     if tranx.type_ == "capital" and tranx.to == environment.firms[0]:
-            #         capital = capital - tranx.amount
-            # print(environment.firms[0].get_account("deposits")+capital-environment.firms[0].get_account("loans"))
-            # capital = 0.0
-            # for tranx in environment.firms[1].accounts:
-            #     if tranx.type_ == "capital" and tranx.from_ == environment.firms[1]:
-            #         capital = capital + tranx.amount
-            #     if tranx.type_ == "capital" and tranx.to == environment.firms[1]:
-            #         capital = capital - tranx.amount
-            # print(environment.firms[1].get_account("deposits")+capital-environment.firms[1].get_account("loans"))
-            # capital = 0.0
-            # for tranx in environment.firms[2].accounts:
-            #     if tranx.type_ == "capital" and tranx.from_ == environment.firms[2]:
-            #         capital = capital + tranx.amount
-            #     if tranx.type_ == "capital" and tranx.to == environment.firms[2]:
-            #         capital = capital - tranx.amount
-            # print(environment.firms[2].get_account("deposits")+capital-environment.firms[2].get_account("loans"))
-            print(environment.banks[0])
-            # print(environment.firms[0])
-        # Close the output file at the end of the simulation
-        measurement.close_file()
     # ------------------------------------------------------------------------
