7a945db6f1c9ef2622f26ad70ee83fe1d09bd730
t1nak <tinakoziol@yahoo.de>
2017-10-18 22:23:48 +0200

    random_shocks
    d

--- examples/firesales_SA/firesale.py
+++ examples/firesales_SA/firesale.py
@@ -34,12 +34,12 @@ if __name__ == '__main__':
     from src.runner import Runner
     import logging
     import pandas as pd
+    import numpy as np
     import sys
 
 # We pass in the name of the environment xml as args[1] here:
     print "The name of the script is ", sys.argv[0]
     # args = []
-
 #
 # INITIALIZATION
 #
@@ -58,54 +58,200 @@ if __name__ == '__main__':
 #
 # UPDATE STEP
 #
-    for i in range(int(environment.static_parameters['num_simulations'])):
+    output = []
+    print environment.static_parameters['num_simulations']
+    mu, sigma = 2., 0.7 # mean and standard deviation
+    mylist = np.random.lognormal(mu, sigma, 2).tolist()
 
-        if i == 0:
+    random_shocks =[i/-100 for i in mylist]
 
-            print("**********START simulation %s") % (i+1)
-            environment.initialize(environment_directory, identifier)
-            environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[1])
+    for index, element in enumerate(random_shocks):
 
-            logging.info('  STARTED with run %s',  str(i))
-            runner.initialize(environment)
-            # do the run
-            runner.do_run(environment)
-            df1 = runner.updater.env_var_par_df
-            logging.info(' Run DONE')
-            logging.info("***\nSimulation number %s had total number of %s sweeps", i+1, str(runner.num_sweeps))
-            logging.info("***\nThis run had the illiquidity coefficient %s " , environment.static_parameters['illiquidity'])
+        index
+        print("**********START simulation %s") % (index+1)
 
-        if i == 1:
-            print("**********START simulation %s") % (i+1)
-            environment.initialize(environment_directory, identifier)
-            environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[2])
+        environment.initialize(environment_directory, identifier)
+        environment.shocks[0].asset_returns[sys.argv[1]] = element
+        print environment.shocks[0].asset_returns[sys.argv[1]]
+        logging.info('  STARTED with run %s',  str(index))
+        runner.initialize(environment)
+        # do the run
+        runner.do_run(environment)
+        df_system = runner.updater.env_var_par_df
 
-            logging.info('  STARTED with run %s',  str(i))
-            runner.initialize(environment)
-            # do the run
-            runner.do_run(environment)
-            df2 = runner.updater.env_var_par_df
-            logging.info(' Run DONE')
-            logging.info("***\nSimulation number %s had total number of %s sweeps", (i+1), str(runner.num_sweeps))
-            logging.info("***\nThis run had the illiquidity coefficient %s ", environment.static_parameters['illiquidity'])
+        df_asset_class = pd.DataFrame({'asset_class': [sys.argv[1]]})
+        df_shock = pd.DataFrame({'shock': [element]})
+        df_all_fucking_variables = pd.concat((df_system , df_asset_class, df_shock), axis=1, ignore_index = False )
 
-        if i == 2:
-            print("**********START simulation %s") % (i+1)
-            environment.initialize(environment_directory, identifier)
-            environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[3])
-            logging.info('  STARTED with run %s',  str(i))
-            runner.initialize(environment)
-            # do the run
-            runner.do_run(environment)
-            df3 = runner.updater.env_var_par_df
+        output.append(df_all_fucking_variables)
 
-            logging.info(' Run DONE')
-            logging.info("***\nSimulation number %s had total number of %s sweeps", (i+1), str(runner.num_sweeps))
-            logging.info("***\nThis run had the illiquidity coefficient %s ", environment.static_parameters['illiquidity'])
-            results_varnames = []
-            for i in runner.sweep_result_list[0]:
-                results_varnames.append(i)
-            df_all = pd.concat([df1, df2, df3], keys=[ float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])], ignore_index = False).to_csv("test.csv")
+        logging.info(' Run DONE')
+        logging.info("***\nSimulation number %s had total number of %s sweeps", index, str(runner.num_sweeps))
+        logging.info("***\nThis run had the illiquidity coefficient %s " , environment.static_parameters['illiquidity'])
+
+    print output
+    pd.concat(output, ignore_index = False).to_csv("test.csv")
+
+
+        # if i == 1:
+        #     print("**********START simulation %s") % (i+1)
+        #     environment.initialize(environment_directory, identifier)
+        #     environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[2])
+        #
+        #     logging.info('  STARTED with run %s',  str(i))
+        #     runner.initialize(environment)
+        #     # do the run
+        #     runner.do_run(environment)
+        #     df2 = runner.updater.env_var_par_df
+        #     logging.info(' Run DONE')
+        #     logging.info("***\nSimulation number %s had total number of %s sweeps", (i+1), str(runner.num_sweeps))
+        #     logging.info("***\nThis run had the illiquidity coefficient %s ", environment.static_parameters['illiquidity'])
+        #
+        # if i == 2:
+        #     print("**********START simulation %s") % (i+1)
+        #     environment.initialize(environment_directory, identifier)
+        #     environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[3])
+        #     logging.info('  STARTED with run %s',  str(i))
+        #     runner.initialize(environment)
+        #     # do the run
+        #     runner.do_run(environment)
+        #     df3 = runner.updater.env_var_par_df
+        #
+        #     logging.info(' Run DONE')
+        #     logging.info("***\nSimulation number %s had total number of %s sweeps", (i+1), str(runner.num_sweeps))
+        #     logging.info("***\nThis run had the illiquidity coefficient %s ", environment.static_parameters['illiquidity'])
+        #     results_varnames = []
+        #     for i in runner.sweep_result_list[0]:
+        #         results_varnames.append(i)
+        #
+        #
+        #
+        #     df_all = pd.concat(list_of_dataframes, keys=[ float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])], ignore_index = False).to_csv("test.csv")
 
     print('Program DONE! Fire-sales happend!')
     logging.info('FINISHED Program logging for run: %s \n', environment_directory + identifier + ".xml")
+
+
+"The code below passes in three specific shocks\
+and the asset class as arguments\
+For example for bonds\
+python firesale.py -0.1 -0.5 -0.4 m_14"
+
+
+    #
+    #
+    # #!/usr/bin/env python
+    # # [SublimeLinter pep8-max-line-length:300]
+    # # -*- coding: utf-8 -*-
+    #
+    # """
+    # This is a minimal example.
+    #
+    # black_rhino is a multi-agent simulator for financial network analysis
+    # Copyright (C) 2012 Co-Pierre Georg (co-pierre.georg@keble.ox.ac.uk)
+    #
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, version 3 of the License.
+    #
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+    #
+    # You should have received a copy of the GNU General Public License
+    # along with this program. If not, see <http://www.gnu.org/licenses/>
+    #
+    # The development of this software has been supported by the ERA-Net
+    # on Complexity through the grant RESINEE.
+    # """
+    #
+    # # -------------------------------------------------------------------------
+    # #
+    # #  MAIN
+    # #
+    # # -------------------------------------------------------------------------
+    # if __name__ == '__main__':
+    #     from src.environment import Environment
+    #     from src.runner import Runner
+    #     import logging
+    #     import pandas as pd
+    #     import sys
+    #
+    # # We pass in the name of the environment xml as args[1] here:
+    #     print "The name of the script is ", sys.argv[0]
+    #     # args = []
+    #
+    # #
+    # # INITIALIZATION
+    # #
+    #     environment_directory = str("configs/environment/")
+    #     identifier = str("firesales")
+    #     log_directory = str("log/")
+    #
+    # ####### Logging Configuration!!!
+    #     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
+    #                         filename=log_directory + identifier + ".log", level=logging.INFO)
+    #     logging.info('The program starts! Logging enabled.')
+    # ############
+    #     environment = Environment(environment_directory, identifier)
+    #     runner = Runner(environment)
+    #
+    # #
+    # # UPDATE STEP
+    # #
+    #     mu, sigma = 2., 0.7 # mean and standard deviation
+    #     shocks = np.random.lognormal(mu, sigma, 10)
+    #
+    #     for i in range(int(environment.static_parameters['num_simulations'])):
+    #
+    #         if i == 0:
+    #
+    #             print("**********START simulation %s") % (i+1)
+    #             environment.initialize(environment_directory, identifier)
+    #             environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[1])
+    #
+    #             logging.info('  STARTED with run %s',  str(i))
+    #             runner.initialize(environment)
+    #             # do the run
+    #             runner.do_run(environment)
+    #             df1 = runner.updater.env_var_par_df
+    #             logging.info(' Run DONE')
+    #             logging.info("***\nSimulation number %s had total number of %s sweeps", i, str(runner.num_sweeps))
+    #             logging.info("***\nThis run had the illiquidity coefficient %s " , environment.static_parameters['illiquidity'])
+    #
+    #         if i == 1:
+    #             print("**********START simulation %s") % (i+1)
+    #             environment.initialize(environment_directory, identifier)
+    #             environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[2])
+    #
+    #             logging.info('  STARTED with run %s',  str(i))
+    #             runner.initialize(environment)
+    #             # do the run
+    #             runner.do_run(environment)
+    #             df2 = runner.updater.env_var_par_df
+    #             logging.info(' Run DONE')
+    #             logging.info("***\nSimulation number %s had total number of %s sweeps", (i+1), str(runner.num_sweeps))
+    #             logging.info("***\nThis run had the illiquidity coefficient %s ", environment.static_parameters['illiquidity'])
+    #
+    #         if i == 2:
+    #             print("**********START simulation %s") % (i+1)
+    #             environment.initialize(environment_directory, identifier)
+    #             environment.shocks[0].asset_returns[sys.argv[4]] = float(sys.argv[3])
+    #             logging.info('  STARTED with run %s',  str(i))
+    #             runner.initialize(environment)
+    #             # do the run
+    #             runner.do_run(environment)
+    #             df3 = runner.updater.env_var_par_df
+    #
+    #             logging.info(' Run DONE')
+    #             logging.info("***\nSimulation number %s had total number of %s sweeps", (i+1), str(runner.num_sweeps))
+    #             logging.info("***\nThis run had the illiquidity coefficient %s ", environment.static_parameters['illiquidity'])
+    #             results_varnames = []
+    #             for i in runner.sweep_result_list[0]:
+    #                 results_varnames.append(i)
+    #
+    #             df_all = pd.concat([df1, df2, df3], keys=[ float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])], ignore_index = False).to_csv("test.csv")
+    #
+    #     print('Program DONE! Fire-sales happend!')
+    #     logging.info('FINISHED Program logging for run: %s \n', environment_directory + identifier + ".xml")
