8cc4226e847f1a70add5c08e1ea82d5f00e0dd88
joerischasfoort <j.schasfoort@rug.nl>
2018-02-12 18:19:16 +0200

    added_portfolio_and_news_functions

--- examples/qe_financial_spillover/src/functions/tests/test_distribute_countries.py
+++ examples/qe_financial_spillover/src/functions/tests/test_distribute_countries.py
@@ -4,7 +4,7 @@ from qe_financial_spillover.src.functions.distribute import *
 """ Helper functions to assign funds to countries """
 
 def test_distribute_funds_equally():
-    assert_equal(distribute_funds_equally(number_funds=2, list_countries=["US", "EU"]), ["US", "EU"])
+    assert_equal(distribute_funds_equally(n_funds=2, list_countries=["US", "EU"]), ["US", "EU"])
 
 def test_weighted_choice():
     assert_equal(weighted_choice(choices=[("Germany", 85), ("SA", 15)]), "Germany")
