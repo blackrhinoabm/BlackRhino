81ae653343a69716368dc928285f289db48c68fd
joerischasfoort <j.schasfoort@rug.nl>
2018-02-13 14:11:41 +0200

    add_stochastic_process_with_tests

--- examples/qe_financial_spillover/src/functions/tests/test_distribute_countries.py
+++ examples/qe_financial_spillover/src/functions/tests/test_distribute_countries.py
@@ -7,6 +7,6 @@ def test_distribute_funds_equally():
     assert_equal(distribute_funds_equally(n_funds=2, list_countries=["US", "EU"]), ["US", "EU"])
 
 def test_weighted_choice():
-    assert_equal(weighted_choice(choices=[("Germany", 85), ("SA", 15)]), "Germany")
+    assert_equal(weighted_choice(choices=[("Germany", 100), ("SA", 0)]), "Germany")
 
 
